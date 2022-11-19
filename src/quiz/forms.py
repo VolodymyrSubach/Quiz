6dd23334fcf500c83e1c10c09cc87dc708c7b573
Navigator <seanavigator2@gmail.com>
2022-11-19 21:06:04 +0200

    Home Work 26

--- src/quiz/forms.py
+++ src/quiz/forms.py
@@ -8,10 +8,34 @@ class QuestionInlineFormSet(forms.BaseInlineFormSet):
     def clean(self):
         if not (self.instance.QUESTION_MIN_LIMIT <= len(self.forms) <= self.instance.QUESTION_MAX_LIMIT):
             raise ValidationError(
-                f'Questions count must be range '
+                f'Questions count must be in range '
                 f'from {self.instance.QUESTION_MIN_LIMIT} '
                 f'to {self.instance.QUESTION_MAX_LIMIT} inclusive'
             )
+
+        order_num_lst = []
+
+        for form in self.forms:
+            if form.cleaned_data['order_num'] > self.instance.QUESTION_MAX_LIMIT:
+                raise ValidationError(
+                    f'Questions number must be not more then '
+                    f'to {self.instance.QUESTION_MAX_LIMIT} inclusive'
+                )
+            if not (1 <= form.cleaned_data['order_num'] <= 100):
+                raise ValidationError(
+                    'Questions number must be between 1 to 100 inclusive'
+                )
+            order_num_lst.append(form.cleaned_data['order_num'])
+            order_num_lst.sort()
+
+        for n in range(len(order_num_lst) - 1):
+            if order_num_lst[0] != 1:
+                raise ValidationError(
+                    'Question numbering does not start from 1')
+            if order_num_lst[n] + 1 != order_num_lst[n + 1]:
+                raise ValidationError(
+                    f'Ascending order of question\'s number {order_num_lst[n]} '
+                    f'and {order_num_lst[n + 1]} does not increase by 1!')
 
 
 class ChoiceInlineFormSet(forms.BaseInlineFormSet):
