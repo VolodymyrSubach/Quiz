3633c86cb586e40e6ade06b6b9c9bfdaf736c2f4
Navigator <seanavigator2@gmail.com>
2022-11-20 15:49:04 +0200

    Lesson 28

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import UpdateView
+from django.views.generic.list import MultipleObjectMixin
 
 from .forms import ChoicesFormSet
 from .models import Exam
@@ -19,24 +20,32 @@ class ExamListView(LoginRequiredMixin, ListView):
     context_object_name = 'exams'  # object_list
 
 
-class ExamDetailView(LoginRequiredMixin, DetailView):
+class ExamDetailView(LoginRequiredMixin, DetailView, MultipleObjectMixin):
     model = Exam
     template_name = 'exam/details.html'
     context_object_name = 'exam'
     pk_url_kwarg = 'uuid'
+    paginate_by = 3
 
     def get_object(self, queryset=None):
         uuid = self.kwargs.get('uuid')
         return self.model.objects.get(uuid=uuid)
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super().get_context_data(object_list=self.get_queryset(), **kwargs)
         try:
             context['best_result'] = Result.objects.filter(exam_id=self.object).order_by('-num_correct_answers')[0]
             context['last_run'] = Result.objects.filter(exam_id=self.object).order_by('-update_timestamp')[0]
+
         except IndexError:
             context['best_result'] = 'N/A'
         return context
+
+    def get_queryset(self):
+        return Result.objects.filter(
+                exam=self.get_object(),
+                user=self.request.user
+            ).order_by('state', '-create_timestamp')
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -65,7 +74,7 @@ class ExamResultCreateView(LoginRequiredMixin, CreateView):
                 kwargs={
                     'uuid': uuid,
                     'res_uuid': result.uuid,
-                    'order_num': 1
+                    # 'order_num': 1
                 }
             )
         )
@@ -75,7 +84,10 @@ class ExamResultQuestionView(LoginRequiredMixin, UpdateView):
     def get_params(self, **kwargs):
         uuid = kwargs.get('uuid')
         res_uuid = kwargs.get('res_uuid')
-        order_num = kwargs.get('order_num')
+        # order_num = kwargs.get('order_num')
+        order_num = Result.objects.filter(
+            uuid=res_uuid, user=self.request.user
+        ).values('current_order_number').first()['current_order_number'] + 1
 
         return uuid, res_uuid, order_num
 
@@ -120,7 +132,47 @@ class ExamResultQuestionView(LoginRequiredMixin, UpdateView):
                 kwargs={
                     'uuid': uuid,
                     'res_uuid': res_uuid,
-                    'order_num': order_num + 1
+                    # 'order_num': order_num + 1
                 }
             )
         )
+
+
+class ExamResultDetailView(LoginRequiredMixin, DetailView):
+    model = Result
+    template_name = 'result/details.html'
+    context_object_name = 'result'
+    pk_url_kwarg = 'uuid'
+
+    def get_object(self, queryset=None):
+        uuid = self.kwargs.get('res_uuid')
+
+        return self.get_queryset().get(uuid=uuid)
+
+
+class ExamResultUpdateView(LoginRequiredMixin, UpdateView):
+    def get(self, request, *args, **kwargs):
+        uuid = kwargs.get('uuid')
+        res_uuid = kwargs.get('res_uuid')
+        user = request.user
+
+        result = Result.objects.get(
+            user=user,
+            uuid=res_uuid,
+            # exam__uuid=uuid,
+        )
+
+        return HttpResponseRedirect(
+            reverse(
+                'quiz:question',
+                kwargs={
+                    'uuid': uuid,
+                    'res_uuid': res_uuid,
+                    # 'order_num': result.current_order_number + 1
+                }
+            )
+        )
+
+
+class ExamResultDeleteView(LoginRequiredMixin, DeleteView):
+    pass
